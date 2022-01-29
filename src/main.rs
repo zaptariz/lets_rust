0dbe66ab4993985bc9a71cf4fce322451c7a3618
Josh Philippe <vadivelleninkumar@gmail.com>
2022-01-29 15:48:19 +0530

    get_a_number_from_user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("my first rust program");
+    println!(" !...Lets play a guessing game...!");
+    println!("Guess your number and  enter it : ");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read a number");
+    println!(" your guessing number {}", guess);
 }
